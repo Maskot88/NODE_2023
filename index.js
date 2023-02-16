8ff083a6f586703484bf3e445094765ef6d7837d
Lymarenko Dmytro <benimaru046@gmail.com>
2023-02-16 14:43:20 +0100

    Add commonJS

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const {NyMayh} = require("./MyMath");
+const {Component} = require("./Component");
+
+
+const result = MyMath.sum(4, 8);
+console.log("result = ", result);
+
+const component1 = new Component();
+component1.render();
