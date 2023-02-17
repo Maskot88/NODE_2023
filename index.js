f119e6d3058817e073d20d56e695e8a91f4985dd
Lymarenko Dmytro <benimaru046@gmail.com>
2023-02-17 13:24:54 +0100

    Add lodash

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 
-const fs = require("fs");
-// console.log(fs.promises);
-// const fs = require("fs/promises");
-// const fs = require("fs").promises;
-console.log(fs);
 
-fs.readFile('./test.txt', {encoding:'utf-8'}, (err, data) => {
-    if (err) throw err;
-    console.log(data)
-})
+const fs = require("fs/promises");
+
+
+fs.readFile('./test.txt', 'utf-8')
+    .then((data)=>{
+        console.log(data);
+        fs.appendFile("./newTest.txt", data+"\nLOREM!!!", 'utf-8')
+    })
+    .catch((err)=>{console.log(err)})
